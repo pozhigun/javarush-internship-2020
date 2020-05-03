cf59c54369e5bc6fffbf620eb3909df21f9f6958
Dmitriy <d.pozhigun@gmail.com>
2020-05-03 20:06:31 +0500

    add UpdateTest

--- src/main/java/com/space/controller/ShipController.java
+++ src/main/java/com/space/controller/ShipController.java
@@ -46,6 +46,16 @@ public class ShipController {
         return shipService.getShipById(id);
     }
 
+    @PostMapping(value = "/rest/ships/{id}")
+    @ResponseBody
+    public Ship updateShip(@RequestBody Ship ship, @PathVariable Long id) {
+        if (!isIdValid(id)) {
+            throw new BadRequestException();
+        }
+
+        return shipService.updateShip(ship, id);
+    }
+
     private Boolean isIdValid(Long id) {
         return id != null &&
                 id == Math.floor(id) &&
--- src/main/java/com/space/service/ShipService.java
+++ src/main/java/com/space/service/ShipService.java
@@ -7,6 +7,7 @@ import com.space.repository.ShipRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.time.ZoneId;
 
 @Service
@@ -60,6 +61,59 @@ public class ShipService {
         }
 
         return shipRepository.getOne(id);
+    }
+
+    @Transactional
+    public Ship updateShip(Ship newShip, Long id) {
+        Ship shipUpdate = getShipById(id);
+
+        if (newShip == null || shipUpdate == null) {
+            throw new BadRequestException();
+        }
+        if (newShip.getName() != null) {
+            if (newShip.getName().length() > 50 ||
+                    newShip.getName().isEmpty()) {
+                throw new BadRequestException();
+            }
+            shipUpdate.setName(newShip.getName());
+        }
+        if (newShip.getPlanet() != null) {
+            if (newShip.getPlanet().length() > 50 ||
+                    newShip.getPlanet().isEmpty()) {
+                throw new BadRequestException();
+            }
+            shipUpdate.setPlanet(newShip.getPlanet());
+        }
+        if (newShip.getShipType() != null) {
+            shipUpdate.setShipType(newShip.getShipType());
+        }
+        if (newShip.getProdDate() != null) {
+            if (newShip.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear() < 2800 ||
+                    newShip.getProdDate().toInstant().atZone(ZoneId.systemDefault()).toLocalDate().getYear() > 3019) {
+                throw new BadRequestException();
+            }
+            shipUpdate.setProdDate(newShip.getProdDate());
+        }
+        if (newShip.getUsed() != null) {
+            shipUpdate.setUsed(newShip.getUsed());
+        }
+        if (newShip.getSpeed() != null) {
+            if (newShip.getSpeed() < 0.01d ||
+                    newShip.getSpeed() > 0.99d) {
+                throw new BadRequestException();
+            }
+            shipUpdate.setSpeed(newShip.getSpeed());
+        }
+        if (newShip.getCrewSize() != null) {
+            if (newShip.getCrewSize() < 1 ||
+                    newShip.getCrewSize() > 9999) {
+                throw new BadRequestException();
+            }
+            shipUpdate.setCrewSize(newShip.getCrewSize());
+        }
+
+        shipUpdate.setRating(computationRating(shipUpdate));
+        return shipRepository.save(shipUpdate);
     }
 
     private Double computationRating(Ship ship) {
